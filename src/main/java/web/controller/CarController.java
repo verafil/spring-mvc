494b63040e3cdc41ee516e96c5cef94e86231b69
verafil <9601604@gmail.com>
2021-07-25 11:06:50 +0300

    2.2.2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.Service;
+import web.service.ServiceImp;
 
 import java.util.stream.Collectors;
 
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 public class CarController {
 
     @Autowired
-    Service service;
+    ServiceImp service;
 
     @GetMapping(value = "/cars")
     public String getCar(@RequestParam(value = "count", required = false) int count, ModelMap model) {
